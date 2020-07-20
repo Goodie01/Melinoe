ab379592b1f384aa067af41c6011434060f49f21
Thomas Goodwin <gitlab@goodiemania.org>
2020-07-20 13:26:11 +1200

    Create example to prove that #12 is resolved

--- examples/src/test/java/org/goodiemania/melinoe/samples/failing/FailureScenarios.java
+++ examples/src/test/java/org/goodiemania/melinoe/samples/failing/FailureScenarios.java
@@ -1,6 +1,7 @@
 package org.goodiemania.melinoe.samples.failing;
 
 import org.goodiemania.melinoe.framework.api.MelinoeTest;
+import org.goodiemania.melinoe.framework.api.exceptions.MelinoeException;
 import org.goodiemania.melinoe.samples.GithubRepoPage;
 import org.goodiemania.melinoe.samples.GithubRepoPullRequestPage;
 import org.junit.jupiter.api.DisplayName;
@@ -25,5 +26,15 @@ public class FailureScenarios extends MelinoeTest {
         getSession().web().navigate().to("https://github.com/Goodie01/Melinoe");
         githubRepoPage.clickPullRequestLink();
         githubRepoPullRequestPage.checkPage();
+    }
+
+    @Test
+    @DisplayName("We can also pick up on random exceptions being thrown")
+    public void exceptionThrownExample() {
+        getSession().web().navigate().to("https://github.com/Goodie01/Melinoe");
+        githubRepoPage.checkPage();
+        githubRepoPage.clickPullRequestLink();
+        githubRepoPullRequestPage.checkPage();
+        throw new MelinoeException("Tada!");
     }
 }
